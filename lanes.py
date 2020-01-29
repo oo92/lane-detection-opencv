9e798d433b37a991a06b6a4e9ed55bbede15ad08
oo92 <onur.toronto@gmail.com>
2020-01-29 14:32:31 -0500

    finished the model using the image

--- lanes.py
+++ lanes.py
@@ -2,6 +2,41 @@ import cv2
 import numpy as np
 
 
+def make_coordinates(image, line_parameters):
+    slope, intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1 * (3/5))
+
+    x1 = int((y1 - intercept) / slope)
+    x2 = int((y2 - intercept) / slope)
+
+    return np.array([x1, y1, x2, y2])
+
+
+def average_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1, x2), (y1, y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+
+    left_fit_average = np.average(left_fit, axis=0)
+    right_fit_average = np.average(right_fit, axis=0)
+
+    left_line = make_coordinates(image, left_fit_average)
+    right_line = make_coordinates(image, right_fit_average)
+
+    return np.array([left_line, right_line])
+
+
 # The function creates a gray-scale of the image and applies GaussianBlur to reduce noise.
 def canny(image):
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
@@ -15,8 +50,7 @@ def display_lines(image, lines):
     lined_image = np.zeros_like(image)
 
     if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line.reshape(4)
+        for x1, y1, x2, y2 in lines:
             cv2.line(lined_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
 
     return lined_image
@@ -33,15 +67,17 @@ def region_of_interest(image):
 
 image = cv2.imread('test_image.jpg')
 
-# Creating a copy of the image
+# Creating a copy of the imagel
 lane_image = np.copy(image)
 
-canny = canny(lane_image)
+canny_image = canny(lane_image)
 
-cropped_image = region_of_interest(canny)
+cropped_image = region_of_interest(canny_image)
 
 lines = cv2.HoughLinesP(cropped_image, 2, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=5)
-line_image = display_lines(lane_image, lines)
+
+averaged_lines = average_slope_intercept(lane_image, lines)
+line_image = display_lines(lane_image, averaged_lines)
 
 combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
 
